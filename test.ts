f0416ecc1fb86998613d8accd7a91ba1243ee94a
Richard Guerre <richardmguerre@gmail.com>
2022-07-20 14:32:53 +0200

    remove test.ts

--- test.ts
+++
@@ -1,9 +0,0 @@
-import { fakerTypes } from './src/fakerTypes';
-import { runFakerUsingPath } from './src/utils';
-// import { faker } from '@faker-js/faker';
-
-for (const fakerType of fakerTypes) {
-  const { type } = fakerType;
-  console.log(type);
-  console.log({ result: runFakerUsingPath(type) });
-}
